f1dc5be4176ac2608aa5bef816f43ddde15b91ae
Robert Hoyt <roberthoytdesign@gmail.com>
2018-02-06 00:50:11 -0700

    tested and confirmed smart contracts

---
+++ contracts/OrmusToken.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.17;
+
+import 'zeppelin-solidity/contracts/token/ERC20/StandardToken.sol';
+
+contract OrmusToken is StandardToken {
+  string public name = 'OrmusToken';
+  string public symbol = 'Orms';
+  uint8 public decimals = 18;
+  uint public INITIAL_SUPPLY = 177000000;
+
+  function OrmusToken() public {
+    totalSupply_ = INITIAL_SUPPLY;
+    balances[msg.sender] = INITIAL_SUPPLY;
+  }
+}
---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,5 @@
+var OrmusToken = artifacts.require("OrmusToken");
+
+module.exports = function(deployer) {
+ deployer.deploy(OrmusToken);
+};
